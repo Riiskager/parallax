d98ba59e6c504847f64dcd8dac32a17e1f5baf5f
MrsBowie <124260064+MrsBowie@users.noreply.github.com>
2025-03-19 12:03:02 +0100

    stjerne og planeter

--- js/parallax.js
+++ js/parallax.js
@@ -1,7 +1,11 @@
-var rellax = new Rellax('.rellax1', {
+var rellax = new Rellax('.neptun', {
     center:true
   });
 
   var rellax = new Rellax('.rellax2', {
+    center:true
+  });
+
+  var rellax = new Rellax('.stjerne', {
     center:true
   });
